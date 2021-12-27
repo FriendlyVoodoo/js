0dc4b02ba0b8d5ab40390cec780e9edee2c40b6d
Andrei Karayani <95904909+FriendlyVoodoo@users.noreply.github.com>
2021-12-27 23:22:07 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const tabs = document.getElementById('tabs'),
-    content = document.querySelectorAll('.content');
+    content = document.querySelectorAll('.content'),
+    tabsDop = document.getElementById('tabs-dop'),
+    contentDop = document.querySelectorAll('.content-dop');
+
 
 const activeContent = el => {
     for (let i = 0; i < tabs.children.length; i++) {
@@ -15,6 +18,24 @@ tabs.addEventListener('click', e => {
         content[i].classList.remove('active');
         if (content[i].dataset.content === currTub) {
             content[i].classList.add('active');
+        }
+    }
+})
+
+const activeContentDop = ele => {
+    for (let i = 0; i < tabsDop.children.length; i++) {
+        tabsDop.children[i].classList.remove('active');
+    }
+    ele.classList.add('active');
+}
+
+tabsDop.addEventListener('click', elem => {
+    const currTubDop = elem.target.dataset.btndop;
+    activeContentDop(elem.target);
+    for (let i = 0; i < contentDop.length; i++) {
+        contentDop[i].classList.remove('active');
+        if (contentDop[i].dataset.contentdop === currTubDop) {
+            contentDop[i].classList.add('active');
         }
     }
 })
